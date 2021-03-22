56d3c2a93617077065d4a524434b9752e7d588db
Your Name <you@example.com>
2021-03-22 09:39:22 +0300

    add search view function and template

--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 from .models import Image
 
-# Create your views here.
 def home(request):
     images = Image.objects.all()
     return render(request, 'home.html', {"images": images})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        images = Image.search_image(category)
+        message = f"{category}"
+
+        return render(request, 'search.html',{"message":message,"images": images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
